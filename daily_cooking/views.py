9a3f22bb197bf91b7b902e05159f28b83bb52c18
yskaaren <karengutierrez0414@gmail.com>
2024-12-19 22:09:14 -0300

    Creacion templates

--- daily_cooking/views.py
+++ daily_cooking/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 # Create your views here.
-def index (request):
-    return render(request, 'index.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')  # Redirige a la vista "inicio" después de iniciar sesión
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    return render(request, 'login.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')  # Renderiza la plantilla "inicio.html"
+
+def ingresar_ingredientes(request):
+    return render(request, 'ingresar_ingredientes.html')  # Página para ingresar ingredientes
+
+def ingresar_restricciones(request):
+    return render(request, 'restricciones.html')  # Redirige a una plantilla de restricciones
